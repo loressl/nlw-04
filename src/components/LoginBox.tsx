a0628627d57e0a735c5330525c4e19dfe939ac1a
loressl <loryssl@hotmail.com>
2021-02-27 18:02:12 -0300

    Refatoração nos estilos.

--- src/components/LoginBox.tsx
+++ src/components/LoginBox.tsx
@@ -1,6 +1,9 @@
+import { useState } from 'react'
 import styles from '../styles/components/LoginBox.module.css'
 
 export function LoginBox() {
+    const [username, setUsername] = useState('')
+
     return (
         <div className={styles.loginBoxContainer}>
             <img src="/logo-full.svg" alt="Símbolo" />
@@ -10,7 +13,17 @@ export function LoginBox() {
                 <p>Faça login com seu Github para começar</p>
             </div>
             <div className={styles.loginBoxInput}>
-                <input placeholder="Digite seu username"/>
+                <input
+                    placeholder="Digite seu username"
+                    value={username}
+                    onChange={(event)=>setUsername(event.target.value)}
+                />
+                <button
+                    type="button"
+                    className={username !== '' ? styles.loginBoxButtonActive: styles.loginBoxButtonInative}
+                >
+                    <img src="/icons/arrow.svg" alt="Enter" />
+                </button>
             </div>
         </div>
     )
